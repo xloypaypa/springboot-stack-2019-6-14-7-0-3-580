9abaf5a1d9118f16201f3ec457825e1e90ac4f7f
xsu <xsu@thoughtworks.com>
2019-06-28 14:52:34 +0800

    can remove employee from data.

--- src/main/java/com/tw/apistackbase/employee/EmployeeDataService.java
+++ src/main/java/com/tw/apistackbase/employee/EmployeeDataService.java
@@ -5,21 +5,30 @@ import com.tw.apistackbase.employee.entity.Gender;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class EmployeeDataService {
 
-    private final List<Employee> employees;
+    private final Map<Integer, Employee> employees;
 
     public EmployeeDataService(List<Employee> employees) {
-        this.employees = employees;
+        this.employees = new HashMap<>();
+        for (Employee employee : employees) {
+            this.employees.put(employee.getId(), employee);
+        }
     }
 
     public List<Employee> getAllEmployees() {
-        return new ArrayList<>(this.employees);
+        return new ArrayList<>(this.employees.values());
     }
 
     public void addEmploy(Employee employee) {
-        this.employees.add(employee);
+        this.employees.put(employee.getId(), employee);
+    }
+
+    public void removeEmployee(int id) {
+        this.employees.remove(id);
     }
 }
--- src/test/java/com/tw/apistackbase/employee/EmployeeDataServiceTest.java
+++ src/test/java/com/tw/apistackbase/employee/EmployeeDataServiceTest.java
@@ -34,4 +34,20 @@ public class EmployeeDataServiceTest {
         except.add(new Employee(1, "Xiaoming", 20, Gender.MALE));
         assertEquals(except, employeeDataService.getAllEmployees());
     }
+
+    @Test
+    public void should_not_have_the_employee_when_delete_it_from_data_service_by_id() {
+        List<Employee> defaultEmployee = new ArrayList<>();
+        defaultEmployee.add(new Employee(1, "Xiaoming", 20, Gender.MALE));
+        defaultEmployee.add(new Employee(2, "Xiaohong", 19, Gender.FEMALE));
+        defaultEmployee.add(new Employee(3, "Xiaozhi", 15, Gender.MALE));
+        EmployeeDataService employeeDataService = new EmployeeDataService(defaultEmployee);
+
+        employeeDataService.removeEmployee(2);
+
+        List<Employee> except = new ArrayList<>();
+        except.add(new Employee(1, "Xiaoming", 20, Gender.MALE));
+        except.add(new Employee(3, "Xiaozhi", 15, Gender.MALE));
+        assertEquals(except, employeeDataService.getAllEmployees());
+    }
 }
